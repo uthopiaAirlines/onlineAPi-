efea6841b33a0274363e67debe7104bc5ae7d7d4
AverySSgc <avery.corbett@smoothstack.com>
2020-05-03 00:33:16 -0400

    fixed bug with authorizations across cors request

--- authorization/jwtAuth.js
+++ authorization/jwtAuth.js
@@ -1,7 +1,11 @@
 const jwt = require('jwt-simple');
 
 exports.authenticate = function (req, res, next) {
-    let token = req.get('Authorization').substring(7);
+    console.log(req.header('accept'));
+    console.log(req.get('accept'));
+    console.log(req.get('authorization'));
+    console.log(req.headers);
+    let token = req.header('authorization').substring(7);
     if (token) {
         let decoded = jwt.decode(token, process.env.JWT_SECRET_TOKEN.replace('/\\n/g', '\n'));
         if (decoded.client_id != process.env.CLIENT_ID) {
